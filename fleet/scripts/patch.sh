327c4a04f1eacc58ae82253d2afde672f0239f66
bartr <bartr@microsoft.com>
2022-03-03 03:35:56 +0000

    added deploy

--- fleet/scripts/patch.sh
+++ fleet/scripts/patch.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "$(date +'%Y-%m-%d %H:%M:%S')  patched" >> /home/akdc/status
 
-sudo sed -i  "s@\/home\/akdc\/akdc\/fleet\/bin@\/home\/akdc\/gitops\/bin@g" /etc/bash.bashrc
-
 echo "$(hostname) patched"
